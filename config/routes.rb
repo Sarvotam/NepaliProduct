bcb6baf8761222e2282fb04d87493e98fd7ea563
Sarvotam <hengajusarvottam@gmail.com>
2018-02-16 13:47:00 +0545

    new

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+
+  get 'home/about_us'
+
+  get 'home/contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
